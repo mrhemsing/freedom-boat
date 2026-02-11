28b2e50c9b91c11db2e3dc63b33afa4058272a66
Matt Hemsing <46225546+mrhemsing@users.noreply.github.com>
2026-02-10 21:44:40 -0800

    Add North Saanich address; remove Port Moody postal code

--- lib/locations.ts
+++ lib/locations.ts
@@ -13,7 +13,7 @@ export const LOCATIONS: Record<LocationId, LocationProfile> = {
   'port-moody': {
     id: 'port-moody',
     name: 'Port Moody',
-    address: '850 Barnet Hwy, Port Moody, BC V3H 1V6',
+    address: '850 Barnet Hwy, Port Moody, BC',
     // Marina anchor (Burrard Inlet)
     lat: 49.291406,
     lon: -122.884611
@@ -21,6 +21,7 @@ export const LOCATIONS: Record<LocationId, LocationProfile> = {
   'north-saanich': {
     id: 'north-saanich',
     name: 'North Saanich',
+    address: '2300 Canoe Cove Rd, North Saanich, BC',
     lat: 48.65,
     lon: -123.43
   }
