86145a55e200dc53baf05ce3a7b9d819f0db04c7
Matt Hemsing <46225546+mrhemsing@users.noreply.github.com>
2026-06-11 10:46:49 -0700

    Tune marina network label style

--- app/location/[locationId]/MarinaJump.tsx
+++ app/location/[locationId]/MarinaJump.tsx
@@ -85,7 +85,7 @@ export default function MarinaJump({
         </span>
       </summary>
       <div ref={panelRef} className="marinaJumpPanel" aria-label="Marina pages">
-        <div className="marinaJumpNetworkLabel" style={{ color: 'rgba(34, 197, 94, 0.65)' }}>
+        <div className="marinaJumpNetworkLabel" style={{ color: 'aqua', opacity: 0.6, fontSize: '12px' }}>
           Freedom Boat Club Network
         </div>
         {groups.map((group) => (
